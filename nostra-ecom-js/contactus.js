762851da62abbc8698a9f62bddf122d2f96b2903
Kowsalya Ranganathan <rkowsalya.in@gmail.com>
2026-01-16 19:56:23 +0530

    Delete nostra-ecom-js directory

--- nostra-ecom-js/contactus.js
+++
@@ -1,9 +0,0 @@
-// Side Nav Bar
-var sideNavMenu = document.getElementById("side-navbar-activate")
-var sideNavBar = document.querySelector(".side-navbar")
-sideNavMenu.addEventListener("click", function () {
-    sideNavBar.style.marginLeft = "0px"
-})
-document.getElementById("side-navbar-close").addEventListener("click", function () {
-    document.querySelector(".side-navbar").style.marginLeft = "-60%"
-})
